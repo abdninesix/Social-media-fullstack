b70b7eca63d8c66983382e37c8400a8a231fb688
abdninesix <mohabd1995@gmail.com>
2025-09-16 14:46:38 +0500

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from 'next'
 import { ClerkProvider } from "@clerk/nextjs";
+import QueryProvider from "@/providers/QueryProvider";
 
 export const metadata: Metadata = {
   title: 'Social Website',
@@ -19,11 +20,13 @@ export default function AppLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body>
-          {children}
-        </body>
-      </html>
+      <QueryProvider>
+        <html lang="en">
+          <body>
+            {children}
+          </body>
+        </html>
+      </QueryProvider>
     </ClerkProvider>
   );
 }
