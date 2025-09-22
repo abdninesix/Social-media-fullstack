6d0543882b391405da9ace36a275927f372e68c8
abdninesix <mohabd1995@gmail.com>
2025-09-22 19:53:03 +0500

    Updated

--- src/app/(board)/[username]/status/[postId]/page.tsx
+++ src/app/(board)/[username]/status/[postId]/page.tsx
@@ -2,11 +2,36 @@ import { prisma } from "@/client";
 import Comments from "@/components/Comments";
 import Image from "@/components/Image";
 import Post from "@/components/Post";
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
-const StatusPage = async ({ params }: { params: Promise<{username: string, postId: string}> }) => {
+const StatusPage = async ({ params }: { params: Promise<{ username: string, postId: string }> }) => {
 
-  const post = await prisma.post.findFirst()
+  const { userId } = await auth();
+  if (!userId) return;
+
+  const postId = (await params).postId
+  const post = await prisma.post.findFirst({
+    where: { id: Number(postId) },
+    include: {
+      user: { select: { displayName: true, username: true, img: true } },
+      _count: { select: { likes: true, rePosts: true, comments: true } },
+      likes: { where: { userId }, select: { id: true } },
+      rePosts: { where: { userId }, select: { id: true } },
+      saves: { where: { userId }, select: { id: true } },
+      comments: {
+        include: {
+          user: { select: { displayName: true, username: true, img: true } },
+          _count: { select: { likes: true, rePosts: true, comments: true } },
+          likes: { where: { userId }, select: { id: true } },
+          rePosts: { where: { userId }, select: { id: true } },
+          saves: { where: { userId }, select: { id: true } },
+        },
+      }
+    },
+  })
+  if (!post) return notFound()
 
   return (
     <div className="">
@@ -16,8 +41,8 @@ const StatusPage = async ({ params }: { params: Promise<{username: string, postI
         </Link>
         <h1 className="font-bold text-lg">Post</h1>
       </div>
-      <Post type="status" />
-      <Comments />
+      <Post type="status" post={post} />
+      <Comments comments={post.comments} postId={post.id} username={post.user.username} />
     </div>
   );
 };
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,22 +1,38 @@
+import { Post as PostType } from "@prisma/client"
 import Image from "./Image"
-import Post from "./Post"
+import Post from "./Post";
 
-const Comments = () => {
+type commentWithDetails = PostType & {
+  user: {
+    displayName: string | null;
+    username: string;
+    img: string | null;
+  };
+  _count: {
+    likes: number;
+    rePosts: number;
+    comments: number;
+  };
+  likes: { id: number }[];
+  rePosts: { id: number }[];
+  saves: { id: number }[];
+}
+
+const Comments = ({ comments, postId, username }: { comments: commentWithDetails[], postId: number, username: string }) => {
   return (
     <div className=''>
       <form className='flex items-center justify-between gap-4 p-4 '>
         <div className='relative w-10 h-10 rounded-full overflow-hidden'>
-          <Image path="sm/general/avatar.png" alt="Abdullah" w={100} h={100} tr={true}/>
+          <Image path="sm/general/avatar.png" alt="Abdullah" w={100} h={100} tr={true} />
         </div>
-        <input type="text" className="flex-1 bg-transparent outline-none p-2 text-xl" placeholder="Post your reply"/>
+        <input type="text" className="flex-1 bg-transparent outline-none p-2 text-xl" placeholder="Post your reply" />
         <button className="py-2 px-4 font-bold bg-white text-black rounded-full">Reply</button>
       </form>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          <Post type="comment" post={comment} />
+        </div>
+      ))}
     </div>
   )
 }
