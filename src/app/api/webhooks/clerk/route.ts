1ea71a40c2f97972927e85099c5153b99240be1e
abdninesix <mohabd1995@gmail.com>
2025-09-22 21:50:19 +0500

    Updated

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,33 @@
+import { prisma } from '@/client'
+import { verifyWebhook } from '@clerk/nextjs/webhooks'
+import { NextRequest } from 'next/server'
+
+export async function POST(req: NextRequest) {
+    try {
+        const evt = await verifyWebhook(req)
+
+        // Do something with payload
+        // For this guide, log payload to console
+        const { id } = evt.data
+        const eventType = evt.type
+        console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+        console.log('Webhook payload:', evt.data)
+
+        //Our action for database
+        if (eventType === "user.created") {
+            try {
+                await prisma.user.create.({
+                    id:
+                })
+            } catch (error) {
+                console.log(error)
+                return new Response('Error creating the user', { status: 500 })
+            }
+        }
+
+        return new Response('Webhook received', { status: 200 })
+    } catch (err) {
+        console.error('Error verifying webhook:', err)
+        return new Response('Error verifying webhook', { status: 400 })
+    }
+}
