e32b07ebb90af83faca43becbf46000e64365ffd
abdninesix <mohabd1995@gmail.com>
2025-09-28 23:52:26 +0500

    Updated

--- src/app/(board)/page.tsx
+++ src/app/(board)/page.tsx
@@ -9,7 +9,7 @@ const Homepage = async () => {
       <div className='px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
         <Link className="pb-3 flex items-center border-b-4 border-iconBlue" href="/">For you</Link>
         <Link className="pb-3 flex items-center" href="/">Following</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">Next.js</Link>
+        <Link className="hidden pb-3 md:flex items-center" href="/">Market</Link>
         <Link className="hidden pb-3 md:flex items-center" href="/">React.js</Link>
         <Link className="hidden pb-3 md:flex items-center" href="/">Tailwind</Link>
         <Link className="hidden pb-3 md:flex items-center" href="/">MongoDB</Link>
