2ca9043ab79245e5cff7da71a5c073653001a7a0
abdninesix <mohabd1995@gmail.com>
2025-09-29 21:35:18 +0500

    Updated

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { Notifications } from './svg'
 import { socket } from '@/socket'
 import Link from 'next/link'
+import { Span } from 'next/dist/trace'
 
 type NotificationType = {
     id: string,
@@ -26,21 +27,29 @@ const Notification = () => {
         }
     }, [])
 
+    const reset = () => {
+        setNotifications([])
+    }
+
     return (
         <div className='relative'>
             <div className="p-2 rounded-full hover:bg-inputGray text-white flex items-center gap-4 cursor-pointer" onClick={() => setOpen(prev => !prev)}>
-                <div className="size-8"><Notifications /></div>
+                <div className="size-8 relative">
+                    <Notifications />
+                    <div className='absolute -right-3 -top-3 bg-iconBlue rounded-full size-6 p-1 text-sm flex items-center justify-center'>{notifications.length}</div>
+                </div>
                 <span className="hidden xxl:inline">Notifications</span>
             </div>
-            {open && (<div className='absolute -right-full p-4 rounded-lg shadow-lg bg-white text-black flex flex-col gap-4 w-max'>
+            {open && (<div className='absolute left-full top-1/2 p-4 rounded-lg rounded-tl-none shadow-lg bg-white text-black flex flex-col gap-4 w-max'>
                 <h1 className='text-xl text-textGray'>Notifications</h1>
                 {notifications.map((n) => (
-                    <Link href={n.link} className='cursor-pointer' key={n.id}>
+                    <Link href={n.link} className='cursor-pointer hover:text-iconBlue' key={n.id}>
                         <b>{n.senderUsername}</b>
                         {" "}
                         {n.type === "like" ? "liked your post" : n.type === "comment" ? "replied to your post" : n.type === "rePost" ? "repost your post" : "followed you"}
                     </Link>
                 ))}
+                <span onClick={(reset)} className={`text-center p-2 rounded-md ${notifications.length > 0 ? "bg-black text-sm text-textGrayLight cursor-pointer" : "text-base text-iconBlue"}`}>{notifications.length === 0 ? "You're all caught up!" : "Mark as read"}</span>
             </div>)}
         </div>
     )
