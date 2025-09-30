e0f6b8cab87e83482b123a4ca41452b0475d9481
abdninesix <mohabd1995@gmail.com>
2025-09-30 19:38:36 +0500

    Updated

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -16,7 +16,7 @@ const Video = ({ path, className }: VideoTypes) => {
       path={path}
       className={className}
       transformation={[
-        { width: "1920", height: "1080", quality: 90 as any },
+        { width: "1920", height: "1080" },
         { raw: "l-text,fs-100,co-white,l-end" },
       ]}
       controls
